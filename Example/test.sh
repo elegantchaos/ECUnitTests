a0e39575a1adfc7bc5ad6900dcfe358b51b47b04
Sam Deane <sam@elegantchaos.com>
2012-02-27 10:18:06 +0000

    tweaked project layout slightly

--- Example/test.sh
+++ Example/test.sh
@@ -4,4 +4,4 @@ base=`dirname $0`
 common="$base/../Scripts"
 source "$common/test-common.sh"
 
-xcodebuild -workspace "ECUnitTestsExample.xcworkspace" -scheme "ECUnitTestsExample" -sdk "$testSDKMac" test | "$common/$testConvertOutput"
+xcodebuild -workspace "ECUnitTests.xcworkspace" -scheme "ECUnitTestsExample" -sdk "$testSDKMac" test | "$common/$testConvertOutput"
