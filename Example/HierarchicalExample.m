c303db1e9eda70f1b5825918305b5df509cc69c5
Sam Deane <sam@elegantchaos.com>
2012-02-28 13:36:50 +0000

    added ability to define hierarchies of test data, and to read test structure from files/folders instead of plists

---
+++ Example/HierarchicalExample.m
@@ -0,0 +1,36 @@
+// --------------------------------------------------------------------------
+//! @author Sam Deane
+//
+//  Copyright 2012 Sam Deane, Elegant Chaos. All rights reserved.
+//  This source code is distributed under the terms of Elegant Chaos's 
+//  liberal license: http://www.elegantchaos.com/license/liberal
+// --------------------------------------------------------------------------
+
+#import "ECParameterisedTestCase.h"
+
+@interface HierarchicalExample : ECParameterisedTestCase
+
+@end
+
+@implementation HierarchicalExample
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)parameterisedTestExample
+{
+    NSLog(@"Example test run with data item: %@", self.parameterisedTestDataItem);
+}
+
+@end
