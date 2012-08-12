755b990b4c6bdb7ac060f37632db0c8c4a06176d
Sam Deane <sam@elegantchaos.com>
2012-08-12 20:18:35 +0100

    moved scripts into example

--- Scripts/test-common.sh
+++
@@ -1,5 +0,0 @@
-export testConfig="Debug"
-export testSDKiOS="iphonesimulator"
-export testSDKMac="macosx"
-export testOptions="clean build TEST_AFTER_BUILD=YES"
-export testConvertOutput="ocunit2junit.rb"
