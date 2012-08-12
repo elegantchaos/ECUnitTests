325750fd4576cae0c7239e699d56457351d7f1dd
Sam Deane <sam@elegantchaos.com>
2012-08-12 20:16:23 +0100

    moved examples into separate repo

--- Example/test.sh
+++
@@ -1,7 +0,0 @@
-echo Testing Mac ECUnitTestsExample
-
-base=`dirname $0`
-common="$base/../Scripts"
-source "$common/test-common.sh"
-
-xcodebuild -workspace "ECUnitTests.xcworkspace" -scheme "ECUnitTestsExample" -sdk "$testSDKMac" test | "$common/$testConvertOutput"
